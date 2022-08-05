9ffc59877d32bd241e917e16a6ed00cae35d4471
maksisya <maxim-platonov@mail.ru>
2022-08-05 16:17:09 +0500

    create table news

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/")
+    date_create = models.DateField(auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
