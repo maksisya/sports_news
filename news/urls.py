eb862952baf3dffe15fd83dbcc33bde152070a4d
maksisya <maxim-platonov@mail.ru>
2022-08-09 15:43:38 +0500

    Создание шаблонов, работа с данными из бд

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('cats/<int:catid>/', categories),  # http://127.0.0.1:8000/cats/1/
+    path('about/', about, name='about'),
 ]
--- news/views.py
+++ news/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
 def index(request):
-    return HttpResponse("Страница приложения news")
+    posts = News.objects.all()
+    return render(request, 'news/index.html', {'posts': posts, 'title': 'Главная страница', 'menu': menu})
+
+def about(request):
+    return render(request, 'news/about.html', {'menu': menu, 'title': 'О нас'})
 
 def categories(request, catid):
     return HttpResponse(f"<h1>Новости по категориям, страница № {catid}</h1>")
